fee6481d671d6ae2772a39ff824d1ef38329fc07
Piotr Borelowski <p.borelowski@partner.samsung.com>
2019-06-05 15:15:16 +0200

    Added unit tests for DcaeBeConstants
    Improved the unit test coverage in the package
    org/onap/sdc/dcae/composition/util
    
    Issue-ID: SDC-2327
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: I42fae4cb59a55340e17e2e96818719116a7a4b35

---
+++ src/test/java/org/onap/sdc/dcae/composition/util/DcaeBeConstantsTest.java
@@ -0,0 +1,61 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP SDC
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.sdc.dcae.composition.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class DcaeBeConstantsTest {
+
+    @Test
+    public void testNull() {
+        // when
+        final DcaeBeConstants.LifecycleStateEnum state = DcaeBeConstants.LifecycleStateEnum.findState(null);
+
+        // then
+        assertNull(state);
+    }
+
+    @Test
+    public void testEmptyString() {
+        // when
+        final DcaeBeConstants.LifecycleStateEnum state = DcaeBeConstants.LifecycleStateEnum.findState("");
+
+        // then
+        assertNull(state);
+    }
+
+    @Test
+    public void testAllValues() {
+        for (DcaeBeConstants.LifecycleStateEnum givenState : DcaeBeConstants.LifecycleStateEnum.values()) {
+
+            // when
+            final DcaeBeConstants.LifecycleStateEnum state = DcaeBeConstants.LifecycleStateEnum.findState(givenState.name());
+
+            // then
+            assertEquals(givenState, state);
+        }
+    }
+}
