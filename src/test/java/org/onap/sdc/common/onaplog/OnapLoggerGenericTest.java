ee1ecc7d1ccf16689e1caa7fe5c0f34d046df0d8
k.kedron <k.kedron@partner.samsung.com>
2019-06-17 11:33:35 +0200

    Fixed import in OnapLoggerAuditTest and OnapLoggerGenericTest.
    Changed the import IOnapLogConfiguration to use new OnapLogConfiguration class after changes in 89570.
    
    Issue-ID: SDC-2327
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I23651fb475a2e2543ba8ceb2b531424c9538ebca

--- src/test/java/org/onap/sdc/common/onaplog/OnapLoggerGenericTest.java
+++ src/test/java/org/onap/sdc/common/onaplog/OnapLoggerGenericTest.java
@@ -26,7 +26,6 @@ import static org.junit.Assert.assertNull;
 
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
-import org.onap.sdc.common.onaplog.interfaces.IOnapLogConfiguration;
 import org.slf4j.MDC;
 import uk.org.lidalia.slf4jtest.LoggingEvent;
 
@@ -47,9 +46,11 @@ public class OnapLoggerGenericTest {
         stopwatch.stop();
 
         // then
-        assertNotNull(MDC.get(IOnapLogConfiguration.MDC_BEGIN_TIMESTAMP));
-        assertNotNull(MDC.get(IOnapLogConfiguration.MDC_END_TIMESTAMP));
-        assertNotNull(MDC.get(IOnapLogConfiguration.MDC_ELAPSED_TIME));
+        assertNotNull(MDC.get(OnapLogConfiguration.MDC_BEGIN_TIMESTAMP));
+        assertNotNull(MDC.get(OnapLogConfiguration.MDC_END_TIMESTAMP));
+        assertNotNull(MDC.get(OnapLogConfiguration.MDC_ELAPSED_TIME));
+
+        MDC.clear();
     }
 
     @Test
@@ -70,6 +71,8 @@ public class OnapLoggerGenericTest {
         log = logs.get(1);
         assertEquals(ERROR, log.getLevel().name());
         assertEquals(SECOND_ERROR_EXPECTED_MESSAGE, log.getMessage());
+
+        MDC.clear();
     }
 
     @Test
